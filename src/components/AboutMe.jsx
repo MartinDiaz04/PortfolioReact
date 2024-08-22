77f6637779ce3b1735c1e4e3d12618bd17e29625
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-22 02:55:27 -0300

    refactor: Update AboutMe component with data from cv.json and add download link for resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import data from '../data/cv.json'
+import { FaRegFilePdf } from 'react-icons/fa'
 
 const AboutMe = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    return (
+        <div id='aboutMe' className='flex items-center justify-center my-10'>
+            <div className='text-center w-4/5 caja p-8'>
+                <h1 className='text-4xl sm:text-6xl my-5'>{data.basics.name}</h1>
+                <h2 className='text-2xl sm:text-4xl mt-12'>{data.basics.label}</h2>
+                <img src={data.basics.image} className='mt-5 mx-auto w-2/4 sm:w-1/4 rounded-full'></img>
+                <p className='text-xl sm:text-2xl text-center my-8 mx-auto w-auto md:w-3/4'>{data.basics.summary}</p>
+                <a
+                    href="MartinDiazPreiero.pdf"
+                    download="Martin Diaz Preiero"
+                    className="mx-auto flex items-center justify-center mb-4 w-1/4 p-3 rounded-lg descargarCV"
+                >                    
+                    Descargar CV
+                    <FaRegFilePdf aria-label="Download CV" className="ml-2" />
+                </a>
+            </div>
+        </div>
+
+    )
 }
 
 export default AboutMe
+
