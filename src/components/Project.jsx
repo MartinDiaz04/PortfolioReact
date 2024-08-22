a8c25c9652450b4f7da49c2a31d5b3f8180a33c0
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-22 04:24:14 -0300

    refactor: Update Project component to include project highlights and improve layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,16 +3,19 @@ import { FaGithub } from "react-icons/fa";
 
 const Project = ({ name, description, highlights, url, github, image }) => {
     return (
-        <div className='m-5 p-3 caja w-auto bg-white shadow-md rounded-lg'>
-            <h2 className='text-3xl sm:text-5xl mt-5 text-center'>{name}</h2>
-            <a href={url}>
+        <div className='flex flex-col m-5 p-3 caja w-auto rounded-lg'>
+            <div className="flex-grow">
+                <h2 className='text-3xl sm:text-5xl mt-5 text-center'>{name}</h2>
                 <img src={image} alt={name} className='w-96 mx-auto mt-5 rounded-lg shadow-md' />
-            </a>
-
-            <p className='text-xl sm:text-2xl text-center my-8 mx-auto w-auto md:w-3/4'>{description}</p>
+                <p className='text-xl sm:text-2xl text-center my-8 mx-auto w-auto md:w-3/4'>{description}</p>
+            </div>
             <div className="flex justify-between items-center mt-5">
                 <div className="flex flex-wrap gap-4">
-                    {highlights.map(h => <p key={h} className='text-lg'>{h}</p>)}
+                    {highlights.map(h => (
+                        <span key={h} className='bg-gray-700 text-white py-1 px-3 rounded-2xl text-sm font-medium'>
+                            {h}
+                        </span>
+                    ))}
                 </div>
                 <div className="flex space-x-4">
                     <a href={url} className="iconsProject">
