82680f382187ac673ef85de92704761643a01eac
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-22 05:04:34 -0300

    refactor: Update Projects component layout and remove unnecessary code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,8 +7,6 @@ const Projects = () => {
       <div className="my-10 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 caja w-5/6 md:w-4/6 ">
         {data.projects.map(p => <Project key={p.url} {...p} />)}
       </div>
-
-
     </div>
   )
 }
