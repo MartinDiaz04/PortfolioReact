b211a7bd376697e1bfb107c15a00537fe962a747
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-22 05:31:19 -0300

    refactor: Update Projects component layout and add project title

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,8 @@ import Project from "./Project"
 
 const Projects = () => {
   return (
-    <div id='projects' className='flex justify-center items-center'>
+    <div id='projects' className='flex flex-col justify-center items-center'>
+      <h1 className="text-3xl sm:text-5xl mt-10">Proyectos</h1>
       <div className="my-10 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 caja w-5/6 md:w-4/6 ">
         {data.projects.map(p => <Project key={p.url} {...p} />)}
       </div>
