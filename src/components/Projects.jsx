728a5a13a4cef6e9795bc7522430fcf39fb799d2
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-22 02:55:36 -0300

    refactor: Update Projects component to render projects dynamically from cv.json

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import data from "../data/cv.json"
+import Project from "./Project"
 
 const Projects = () => {
   return (
-    <div>
-      
+    <div id='projects' className='grid grid-cols-2 sm:grid-cols-3'>
+      {data.projects.map(p => <Project key={p.id} {...p} />)}
+
     </div>
   )
 }
