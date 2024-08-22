961d7e94a0043b25451debcab833649d3eee3b17
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-22 04:07:50 -0300

    refactor: Update Project component to include project highlights and improve layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,30 @@
-const Project = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { GrDeploy } from "react-icons/gr";
+import { FaGithub } from "react-icons/fa";
 
-export default Project
+const Project = ({ name, description, highlights, url, github, image }) => {
+    return (
+        <div className='m-5 p-3 caja w-auto bg-white shadow-md rounded-lg'>
+            <h2 className='text-3xl sm:text-5xl mt-5 text-center'>{name}</h2>
+            <a href={url}>
+                <img src={image} alt={name} className='w-96 mx-auto mt-5 rounded-lg shadow-md' />
+            </a>
+
+            <p className='text-xl sm:text-2xl text-center my-8 mx-auto w-auto md:w-3/4'>{description}</p>
+            <div className="flex justify-between items-center mt-5">
+                <div className="flex flex-wrap gap-4">
+                    {highlights.map(h => <p key={h} className='text-lg'>{h}</p>)}
+                </div>
+                <div className="flex space-x-4">
+                    <a href={url} className="iconsProject">
+                        <GrDeploy className='text-2xl' />
+                    </a>
+                    <a href={github} className="iconsProject">
+                        <FaGithub className='text-2xl' />
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,11 @@ import Project from "./Project"
 
 const Projects = () => {
   return (
-    <div id='projects' className='grid grid-cols-2 sm:grid-cols-3'>
-      {data.projects.map(p => <Project key={p.id} {...p} />)}
+    <div id='projects' className='flex justify-center items-center'>
+      <div className="my-10 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 caja w-5/6 sm:w-4/6 ">
+        {data.projects.map(p => <Project key={p.url} {...p} />)}
+      </div>
+
 
     </div>
   )
