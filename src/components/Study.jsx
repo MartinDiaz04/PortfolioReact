0a9682787d4ae6e75c61bd189f4cdf0ff508b758
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-23 01:14:29 -0300

    refactor: Update Study component padding for improved layout consistency

--- src/components/Study.jsx
+++ src/components/Study.jsx
@@ -3,7 +3,7 @@
 const Study = ({ institution, area, startDate, endDate, description }) => {
     return (
 
-        <div className='m-5 p-3 caja'>
+        <div className='m-5 p-1 caja'>
             <h2 className='text-xl sm:text-3xl mt-8'>{institution}</h2>
             <p className='text-xl sm:text-3xl text-center my-8 mx-auto w-auto md:w-3/4'>{area}</p>
             <p className='text-lg sm:text-xl text-center my-8 mx-auto w-auto md:w-3/4'>{startDate} - {endDate}</p>
