04d994949b22efe9fcd652b8473aa725e1747c01
MartinDiaz04 <martinpreierodiaz@gmail.com>
2024-08-23 01:14:43 -0300

    refactor: Update AboutMe component font size for summary

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,11 +5,11 @@ const AboutMe = () => {
 
     return (
         <div id='aboutMe' className='flex items-center justify-center my-10'>
-            <div className='text-center w-5/6 md:w-4/6 caja p-8'>
-                <h1 className='text-4xl sm:text-6xl my-5'>{data.basics.name}</h1>
-                <h2 className='text-2xl sm:text-4xl mt-12'>{data.basics.label}</h2>
+            <div className='text-center w-5/6 md:w-4/6 caja md:p-8'>
+                <h1 className='text-4xl sm:text-5xl my-5'>{data.basics.name}</h1>
+                <h2 className='text-3xl sm:text-4xl mt-12'>{data.basics.label}</h2>
                 <img src={data.basics.image} className='mt-5 mx-auto w-2/4 lg:w-1/4 rounded-full'></img>
-                <p className='text-lg sm:text-2xl text-center my-8 mx-auto w-auto md:w-3/4'>{data.basics.summary}</p>
+                <p className='text-lg sm:text-2xl p-3 text-center my-8 mx-auto w-auto md:w-3/4'>{data.basics.summary}</p>
                 <a
                     href="MartinDiazPreiero.pdf"
                     download="Martin Diaz Preiero"
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,35 +1,47 @@
-import { FaUser } from "react-icons/fa";
-import { FaBriefcase } from "react-icons/fa";
-import { FaGraduationCap } from "react-icons/fa";
+import { FaUser, FaBriefcase, FaGraduationCap } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { setLanguage } from "../../features/languageSlice";
+import Switch from '@mui/material/Switch';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const language = useSelector(state => state.language);
+  const dispatch = useDispatch();
+  const [checked, setChecked] = useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+    console.log(event.target.checked);
+  }
+
   return (
     <div className='header mb-10'>
-      <header className="h-auto flex items-center justify-center caja">
-        <div className="items-center justify-center h-auto">
-          <nav className="w-auto p-5">
-            <ul className="list-none sm:flex sm:items-center sm:justify-center h-auto m-auto">
+      <header className="h-auto xl:h-20 flex flex-col sm:flex-row items-center justify-between caja">        
+        <div className="w-full">
+          <nav className="w-full sm:w-auto p-5">
+            <ul className="list-none flex flex-col sm:flex-row items-center justify-center h-auto m-auto">
               <li className="p-4 list-none mx-10">
-                <a href="#aboutMe" className="w-10 text-center">
-                  <FaUser className="text-4xl sm:text-5xl icon" />
-                </a>
-              </li>              
-              <li className=" p-4 list-none mx-10">
-                <a href="#studies" className="w-10 text-center">
-                  <FaGraduationCap className="text-4xl sm:text-5xl icon" />
+                <a href="#aboutMe" className="w-6 2xl:w-10 text-center">
+                  <FaUser className="text-4xl 2xl:text-5xl  icon" />
                 </a>
               </li>
               <li className="p-4 list-none mx-10">
-                <a href="#projects" className="w-10 text-center">
-                  <FaBriefcase className="text-4xl sm:text-5xl icon" />
+                <a href="#studies" className="w-6 2xl:w-10 text-center">
+                  <FaGraduationCap className="text-4xl 2xl:text-5xl icon" />
+                </a>
+              </li>
+              <li className="p-4 list-none mx-10">
+                <a href="#projects" className="w-6 2xl:w-10 text-center">
+                  <FaBriefcase className="text-4xl 2xl:text-5xl icon" />
                 </a>
               </li>
             </ul>
           </nav>
-        </div>        
+        </div>
+        
       </header>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
